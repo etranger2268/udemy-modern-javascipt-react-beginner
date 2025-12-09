c286b4fa7223ebd1ea7e7a37a8a3fd1750de3fd5
etranger2268 <shun.5.2268@gmail.com>
2025-12-09 14:51:18 +0900

    develop-5-37

--- src/main.js
+++ src/main.js
@@ -15,8 +15,27 @@ const onClickAdd = () => {
   p.className = 'm-1.5';
   p.innerText = inputText;
 
+  // buttonタグ生成
+  const completeButton = document.createElement('button');
+  completeButton.innerText = '完了';
+  completeButton.className =
+    'bg-gray-100 py-1 px-4 mx-1 rounded-xl hover:bg-gray-200 hover:cursor-pointer';
+  completeButton.addEventListener('click', () => {
+    alert('完了');
+  });
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '完了';
+  deleteButton.className =
+    'bg-gray-100 py-1 px-4 mx-1 rounded-xl hover:bg-gray-200 hover:cursor-pointer';
+  deleteButton.addEventListener('click', () => {
+    alert('削除');
+  });
+
   // liタグの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
 
   // 未完了リストに追加

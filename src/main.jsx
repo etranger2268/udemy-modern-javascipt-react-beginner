bce880125f282fa1d01b52f0b19f48460eab5e4f
etranger2268 <shun.5.2268@gmail.com>
2025-12-10 13:53:00 +0900

    develop-6-44

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react';
+import { Fragment, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
 
-const root = createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは!</h1>
+      <p>お元気ですか?</p>
+    </>
+  );
+};
 
 root.render(
   <StrictMode>

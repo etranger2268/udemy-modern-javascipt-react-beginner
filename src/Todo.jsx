bb5d8d30f316c4aa0322a3614ed6f140bab9e2d8
etranger2268 <shun.5.2268@gmail.com>
2025-12-11 16:21:41 +0900

    develop-7-56

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
 export default function Todo() {
+  const [incompleteTodos, setIncompleteTodos] = useState(['TODOです1', 'TODOです2']);
+  const [completeTodos, setCompleteTodos] = useState(['TODOでした1', 'TODOでした2']);
+
   return (
     <>
       <div className="m-2 max-w-md rounded bg-green-200 p-2">
@@ -17,56 +22,43 @@ export default function Todo() {
       <div className="m-2 min-h-52 max-w-md rounded-xl border border-green-500 p-2">
         <p className="mt-0 mb-3 text-center font-black">未完了のTODO</p>
         <ul className="ml-6 list-disc">
-          <li>
-            <div className="flex items-center space-x-1">
-              <p class="m-1.5">TODOです</p>
-              <button
-                type="button"
-                className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
-              >
-                完了
-              </button>
-              <button
-                type="button"
-                className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
-              >
-                削除
-              </button>
-            </div>
-          </li>
-          <li>
-            <div className="flex items-center space-x-1">
-              <p class="m-1.5">TODOです</p>
-              <button
-                type="button"
-                className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
-              >
-                完了
-              </button>
-              <button
-                type="button"
-                className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
-              >
-                削除
-              </button>
-            </div>
-          </li>
+          {incompleteTodos.map((todo) => (
+            <li key={todo}>
+              <div className="flex items-center space-x-1">
+                <p class="m-1.5">{todo}</p>
+                <button
+                  type="button"
+                  className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
+                >
+                  完了
+                </button>
+                <button
+                  type="button"
+                  className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
+                >
+                  削除
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="m-2 min-h-52 max-w-md rounded-xl border border-green-500 bg-green-100 p-2">
         <p className="mt-0 mb-3 text-center font-black">完了のTODO</p>
         <ul className="ml-6 list-disc">
-          <li>
-            <div className="flex items-center space-x-1">
-              <p class="m-1.5">TODOでした</p>
-              <button
-                type="button"
-                className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
-              >
-                戻す
-              </button>
-            </div>
-          </li>
+          {completeTodos.map((todo) => (
+            <li key={todo}>
+              <div className="flex items-center space-x-1">
+                <p class="m-1.5">{todo}</p>
+                <button
+                  type="button"
+                  className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
+                >
+                  戻す
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </>
